3ce59c48fde363b81ac1ded3fa3e06e050af1443
Noaki <49065396+tianbing111@users.noreply.github.com>
2022-10-27 18:46:09 +0800

    Add files via upload

---
+++ hashmap/build/test/CMakeFiles/hashmap_test.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for hashmap_test.
+# This may be replaced when dependencies are built.
---
+++ hashmap/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/noaki/hashmap/test
+# Build directory: /home/noaki/hashmap/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(hashmap_test "/home/noaki/hashmap/build/test/hashmap_test")
+set_tests_properties(hashmap_test PROPERTIES  _BACKTRACE_TRIPLES "/home/noaki/hashmap/test/CMakeLists.txt;9;add_test;/home/noaki/hashmap/test/CMakeLists.txt;0;")
